28d72cdd406d7a912bd5fa28552226a28c245ed9
Annie Phan <anniephan.atp@gmail.com>
2019-03-02 09:48:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,6 @@
 def reverse_each_word(string)
   array1 = string.split(/ /)
-  array2 = []
-  array1.each do |word|
-    array2 << word.reverse
+  array1.collect do |word|
+    array1 << word.reverse
   end
-  return array2.join(" ")
 end
